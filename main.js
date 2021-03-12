6f1ee3a1fac056f8557678d1a83742b425d7383e
Rayansh2010 <77624468+Rayansh2010@users.noreply.github.com>
2021-03-12 09:11:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = new fabric.Canvas('canvas');
+
+var player_x = 15;
+var player_y = 60;
+
+var part_height = 90;
+var part_width = 90;
+
+var part_object = "";
+var player_object = "";
+
+function player_update() {
+    fabric.Image.fromURL("player.png", function(Img) {
+    player_object = Img;
+
+    player_object.scaleToWidth(150);
+    player_object.scaleToHeight(140);
+
+    player_object.set({
+    top: player_y,
+    left: player_x
+    });
+    canvas.add(player_object);
+    });
+
+}
+
+function new_image(get_image) {
+    fabric.Image.fromURL(get_image, function(Img) {
+        block_object = Img;
+    
+        block_object.scaleToWidth(block_width);    
+        block_object.scaleToHeight(block_height);
+    
+        block_object.set({
+        top: player_y,
+        left: player_x
+        });
+        canvas.add(block_object);
+        });
+    
+}
